601b045aa8d479673150fa716d05287bfb0070d4
OverDSlayer <u22309289@utp.edu.pe>
2025-11-26 20:32:28 -0500

    clase Producto añadida

--- munay-sukha-backend/src/main/java/munay_sukha_backend/app/model/Producto.java
+++ munay-sukha-backend/src/main/java/munay_sukha_backend/app/model/Producto.java
@@ -1,5 +1,31 @@
 package munay_sukha_backend.app.model;
 
-public class Producto {
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Table(name="productos")
+@Data
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(length = 500)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private Double precio;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = false)
+    private String categoria;
+
+    @Column(length = 255)
+    private String urlImagen;
 }
