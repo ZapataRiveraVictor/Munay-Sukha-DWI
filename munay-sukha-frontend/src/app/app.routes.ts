9109294650eb9f2230ae4bd5de30a773d49099d0
OverDSlayer <u22309289@utp.edu.pe>
2025-11-27 14:08:14 -0500

    admin

--- munay-sukha-frontend/src/app/app.routes.ts
+++ munay-sukha-frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { CartComponent } from './pages/cart/cart';
 import { CheckoutComponent } from './pages/checkout/checkout';
+import { AdminComponent } from './pages/admin/admin';
 
 export const routes: Routes = [
   { path: '', component: Home }, // Ruta raíz
   { path: 'login', component: Login },
   { path: 'carrito', component: CartComponent },
-  { path: 'checkout', component: CheckoutComponent }, 
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'admin', component: AdminComponent }, 
   { path: '**', redirectTo: '' } // Cualquier ruta desconocida va al home
 ];
